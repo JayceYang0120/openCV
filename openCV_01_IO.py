f96394308d85f3e8f92d00d5f57522c623afa5f3
Jayce Yang <112891631+JayceYang0120@users.noreply.github.com>
2022-09-13 15:44:57 +0800

    Update and rename class_01_IO.py to openCV_01_IO.py

---
+++ openCV_01_IO.py
@@ -0,0 +1,18 @@
+import cv2
+
+img = cv2.imread("colorcolor.jpg")
+
+img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+cv2.imshow("img", img)
+cv2.waitKey(1000)
+
+cap = cv2.VideoCapture("thumb.mp4")
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (0, 0), fx=0.4, fy=0.4)
+    if ret:
+        cv2.imshow("video", frame)
+    else:
+        break
+    if cv2.waitKey(10) == ord('q'):
+        break
